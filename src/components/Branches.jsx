a37856da4e465c0e716387f5cc919958e86f6d98
Developer294 <juandelestal1994@gmail.com>
2024-04-19 17:31:30 -0300

    contact-section ok

--- src/components/Branches.jsx
+++ src/components/Branches.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import mainSucursal from '../assets/main-sucursal.mp4';
+import secondSucursal from '../assets/second-sucursal.mp4';
 import '../styles/branches.css';
 
 export const Branches = () => {
   const [showWelcome, setShowWelcome] = useState(false);
+  const [contentVisible, setContentVisible] = useState(0);
 
   useEffect(() => {
     const delay = setTimeout(() => {
@@ -12,15 +15,64 @@ export const Branches = () => {
     return () => clearTimeout(delay);
   }, []);
 
+  const handleRightArrowClick = () => {
+    if (contentVisible < 1) setContentVisible(prevValue => prevValue + 1);
+  };
+
+  const handleLeftArrowClick = () => {
+    if (contentVisible === 1) setContentVisible(prevValue => prevValue - 1);
+  };
+
   return (
     <>
       <section className={`branch-section ${showWelcome ? 'show' : ''}`}>
-        <div className="branch-content">
-          <h2 className='title'>Welcome to Fuji Sushi</h2>
-          <p className='branch-p'>
-            We are the best option when it comes to sushi. We have the most experienced sushi man on the market. Our ratings speak for themselves 🥢🍣.
-          </p>
-          <button>Learn More</button>
+        {contentVisible === 0 && (
+          <>
+            <video className="rolls-video" autoPlay muted loop>
+              <source src={mainSucursal} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            <div className="branch-content">
+              <h2 className='h2-main-room'>Main Room</h2>
+              <div className='div-main'>
+                <p className="main-sucursal">
+                  <i>Ocean Breeze Avenue, 123</i>
+                  <br/>
+                  Seaside Plaza
+                  <br/>
+                  Sushi City, CA 90210
+                  <br/>
+                  United States
+                </p>
+              </div>
+            </div>
+          </>
+        )}
+        {contentVisible === 1 && (
+          <>
+            <video className="nigiri-video" autoPlay muted loop>
+              <source src={secondSucursal} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            <div className="branch-content">
+              <h2 className='h2-second-room'>Second Room</h2>
+              <div className='div-second'>
+                <p className="second-sucursal">
+                  <i>Sakura Street, 456</i>
+                  <br/>
+                  Wasabi Tower
+                  <br/>
+                  Sushi City, CA 90211
+                  <br/>
+                  United States
+                </p>
+              </div>
+            </div>
+          </>
+        )}
+        <div className="arrow-container">
+          <div className="arrow arrow-left" onClick={handleLeftArrowClick}></div>
+          <div className="arrow arrow-right" onClick={handleRightArrowClick}></div>
         </div>
       </section>
     </>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/contact.css';
+
 export const Contact = () => {
+  const [showWelcome, setShowWelcome] = useState(false);
+  const [showText, setShowText] = useState(false)
+
+  useEffect(() => {
+    const welcomeDelay = setTimeout(() => {
+      setShowWelcome(true);
+    }, 100);
+
+    const textDelay = setTimeout(() => {
+      setShowText(true);
+    }, 300)
+
+    return () => clearTimeout(welcomeDelay, textDelay);
+  }, []);
+
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <section className={`contact-section ${showWelcome ? 'show' : ''}`}>
+        <div className={`contact-content ${showText ? 'show' : ''}` }>
+          <form>
+            <div className="form-group">
+              <label className='label-form' htmlFor="exampleInputEmail1">Email address</label>
+              <input className='form-control input-contact' type="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+            </div>
+            <div className="form-group">
+              <label className='label-form' htmlFor="exampleInputPassword1">Subject</label>
+              <input className='form-control input-contact' type="password" id="exampleInputPassword1" placeholder="Password" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="contact-text-area"></label>
+              <textarea id="contact-text-area" cols="50" rows="3"></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
