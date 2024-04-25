29509662b36340fa7eed3a2f4f763f37335825d8
Developer294 <juandelestal1994@gmail.com>
2024-04-25 20:49:03 -0300

    video config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,11 @@ import { Home } from './Home';
 import { Menu } from './Menu';
 
 const App = () => {
-
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/Fuji-Sushi-Front/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
         </Routes>
       </Router>
